7ff61d84a199e9d5470fcab0d135499bfdf43706
sansv8 <amith.p.pang@gmail.com>
2022-01-31 13:37:57 -0600

    Set up Player.cpp by adding intro comments for the file and including Player.h to the file.

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,10 @@
-
+/** -----------------------------------------------------------------------------
+ *
+ * @file  Player.cpp
+ * @authors Amith Panuganti 
+ * Assignment:   EECS-448 Project #1 
+ * @brief This file impliment the methods of Player
+ * @date 1/31/21
+ *	
+ ---------------------------------------------------------------------------- **/
+ #include "Player.h"
