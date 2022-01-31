e65e6e5f549a840896fa022afae9fa44433438d6
sansv8 <amith.p.pang@gmail.com>
2022-01-31 12:02:39 -0600

    Add setters, getters, and a deconstructors to Player.h. Some setters and getters must be update to work correctly. Additionally, some setters and getters may not be used and can be removed in the future.

--- Player.h
+++ Player.h
@@ -26,12 +26,40 @@ class Player
     int m_shipCounter;
 
     public:
+    //Constructors
     Player();
     Player(int size);
+
+    //Deconstructor
+    ~Player();
+
+    //Getters
+    Board getVisibleBaord();
+    Board getInvisibleBoard();
+    int getNumberOfShips();
+    int getShipCounter();
+    Ship** getShips();
+
+    //Setters
+    //Notes: Use these functions if we construct
+    //the Player class without passing a parameter
+    void setNumberOfShips(int numberOfShips);
+    void setShipCounter(int shipCounter);
+    //Set ships if there is a shipCounter and number of ships
+    void setShips(); 
+    //Set ships by passing in an array of Ship
+    void setShips(Ship** ships);
+    void setVisibleBoard(Board visibleBoard&);
+    void setInvsibleBoard(Board invisibleBoard&);
+
+    //Main Functions
+    //Set Up
     bool isValid(int startRow, int startCol, int endRow, int endCol, int size);
     void placeShip(Ship ship);
-    void view();
     void printSetup();
+
+    //Game Play
+    void view();
     void shipHealthBar();
     void sinkShip(int hitShip);
     void markFriendly(char strike, int row, int col);
