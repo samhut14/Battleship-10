cbef01b45050c54ef701cd57e95d8eb0d777e0ca
sansv8 <amith.p.pang@gmail.com>
2022-01-31 11:25:28 -0600

    Created a branch called playerH to set up Player.h. Set up all variables, and functions for Player.h. However, some variables and functions must be sorted again to be either private or public. Also, the parameters of some functions must be updated in case they are incorrect.

--- Player.h
+++ Player.h
@@ -1,1 +1,40 @@
+/** -----------------------------------------------------------------------------
+ *
+ * @file  Player.h
+ * @authors Amith Panuganti 
+ * Assignment:   EECS-448 Project #1 
+ * @brief This file contains the methods and variables of the Player class
+ *        The Player plays Battleship by setting up their board, attacking the
+ *        other's player board, getting attacked, and either winning or losing
+ * @date 1/31/21
+ *	
+ ---------------------------------------------------------------------------- **/
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "Board.h"
+#include "Ship.h"
+#include <iostream>
+#include <string>
 
+class Player
+{
+    private:
+    Board m_visibleBoard;
+    Board m_invisibleBoard;
+    int m_numberOfShips;
+    Ship** m_ships;
+    int m_shipCounter;
+
+    public:
+    Player();
+    Player(int size);
+    bool isValid(int startRow, int startCol, int endRow, int endCol, int size);
+    void placeShip(Ship ship);
+    void view();
+    void printSetup();
+    void shipHealthBar();
+    void sinkShip(int hitShip);
+    void markFriendly(char strike, int row, int col);
+    void markHostile(char strike, int row, int col);
+};
+#endif
