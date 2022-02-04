b388a7c64b45ac1511075e5d86642b3de88fd1fc
Sam <sam.hutfles@gmail.com>
2022-02-03 20:16:34 -0600

    Some modifications to the class eg removing of currentPlayer variable.

--- Game.h
+++ Game.h
@@ -1,20 +1,20 @@
 #ifndef GAME.H
 #define GAME.H
+#include <iostream>
 #include "Player.h"
 
 class Game
 {
     private:
-    int currentPlayer;
     int numShips;
     Player player1;
     Player player2;
 
     public:
     void setup();
-    void turn(int currentPlayer);
-    char attack(Player attackingPlayer, Player defendingPlayer, int row, int col);
-    bool validAttack(Player attackingPlayer, Player defendingPlayer, int row, int col);
+    void turn(Player attackingPlayer, Player defendingPlayer);
+    bool validAttack(Player attackingPlayer, int row, char col);
+    char attack(Player attackingPlayer, Player defendingPlayer, int row, char col);
     bool gameover();
     void play();
 
