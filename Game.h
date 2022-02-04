d45505051a330e1428035cb8472b0e104c82618b
Sam <sam.hutfles@gmail.com>
2022-02-03 18:06:40 -0600

    creating header file for the Game class

--- Game.h
+++ Game.h
@@ -1,1 +1,22 @@
+#ifndef GAME.H
+#define GAME.H
+#include "Player.h"
 
+class Game
+{
+    private:
+    int currentPlayer;
+    int numShips;
+    Player player1;
+    Player player2;
+
+    public:
+    void setup();
+    void turn(int currentPlayer);
+    char attack(Player attackingPlayer, Player defendingPlayer, int row, int col);
+    bool validAttack(Player attackingPlayer, Player defendingPlayer, int row, int col);
+    bool gameover();
+    void play();
+
+};
+#endif
