97e0b7602cffdcff261ac2c9dc443bc45f088c35
EyeOnYou15 <lakhaniayaan@gmail.com>
2022-02-05 13:42:58 -0600

    Board and ship class updated

--- Ship.cpp
+++ Ship.cpp
@@ -1,1 +1,88 @@
-
+#include "Ship.h"
+using namespace std;
+Ship::Ship(int Size, string* locationArr)
+{
+    Size = size;
+    posArr = locationArr;
+    posArr = nullptr;
+    for(int i = 0; i <= size; i++)
+    {
+       for (int j = 0; i <=  size; i++)
+       {
+           posArr[i][j];
+       }
+       
+    }
+}
+Ship::Ship()
+{
+}
+Ship::~Ship()
+{
+    for (int i = 0; i <= size; i++)
+    {
+        delete[] posArr;
+    }
+    
+}
+void Ship::setLife(int l)
+{
+    l = life;
+}
+void Ship::setSize(int s)
+{
+    s = size;
+}
+void Ship::setAlive(bool a)
+{
+    a = alive;
+}
+int Ship::getLife()
+{
+    return life;
+}
+int Ship::getSize()
+{
+    return size;
+}
+bool Ship::getAlive()
+{
+    return alive;
+}
+bool Ship::loselife(int row, int col)
+{
+   for (int i = 0; i <= size; i++)
+   {
+       for (int j = 0; j <= size; i++)
+       {
+           if(posArr[row][col] == 'S')
+           {
+               life--;
+               return true; 
+           }
+           else
+           {
+               return false;
+           }
+       }
+       
+   }
+   return true;
+   
+}
+void Ship::setColumn(int c)
+{
+    c = column;
+}
+void Ship::setRow(int r)
+{
+    r = row;
+}
+int Ship::getColumn(int index)
+{
+    return (posArr[index].at(1)-65);
+}
+int Ship::getRow(int index)
+{
+    return (posArr[index].at(1)-48);
+}
--- Ship.h
+++ Ship.h
@@ -1,1 +1,30 @@
-
+#ifndef SHIP_H
+#define SHIP_H
+#include <string>
+using namespace std;
+class Ship
+{
+    private:
+    int column;
+    int row;
+    int size;
+    int life;
+    bool alive;
+    string* posArr;
+    public:
+    Ship(int Size, string* locationArr);
+    Ship();
+    ~Ship();
+    void setLife(int l);
+    void setSize(int s);
+    void setAlive(bool a);
+    int getSize();
+    int getLife();
+    bool getAlive();
+    bool loselife(int row, int col);
+    void setColumn(int c);
+    void setRow(int r);
+    int getColumn(int index);
+    int getRow(int index);  
+};
+#endif
