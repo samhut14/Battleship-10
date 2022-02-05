930a2ea86a93031be24b8c1e9e9ffb5d57a2faf1
sansv8 <amith.p.pang@gmail.com>
2022-02-04 18:43:11 -0600

    Added markHostile and view. Changes are stil needed.

--- Player.cpp
+++ Player.cpp
@@ -55,7 +55,6 @@ void sinkShip(int hitship)
     //First, we check if hitship
     //hitship is valid if it is less than or equal to the number of ships
     //and it is greater than 0
-    //histship is also valid if we are not sunking a ship that is already sunk
     //We also need to check if the ship is not alive before sinking it
     if(hitship <= 0 || hitship > m_numberOfShips)
     {
@@ -114,12 +113,17 @@ void sinkShip(int hitship)
         m_visibleBoard.set(row, col, "X");
     }
 
-    //Leave for now
+    //Next, decrease the shipCounter to show that the player lost a ship
+    m_shipCounter--; 
+
+    //TODO:
     //Come back to print message about ship getting sinked
+    //Add any error messages if needed for the correct string
+    //sinkShip is actually Ahmni's Jurisdiction, so delete this if needed
 }
 
 //Marks the player's invisible board
-void Player:markFriendly(std::string strike, int row, int col)
+void Player::markFriendly(std::string strike, int row, int col)
 {
     //Firstly, we need to check if row and col are valid and strike
     //is a valid character
@@ -151,6 +155,91 @@ void Player:markFriendly(std::string strike, int row, int col)
     //Otherwise, continue the program
     //Set the string at row,col in the player's invisble board to be strike
     m_invisibleBoard.set(row, col, strike);
+
+    //TODO: Add any messages if needed.
+    //Change any functions to correct functios for Board
 }
 
+//Marks the player's visible board
+void Player::markHostile(std::string strike, int row, int col)
+{
+    //Firstly, we need to check if row and col are valid and strike
+    //is a valid character
+    //If row is less than 0 or greater than 9
+    if(row < 0 || row > 9)
+    {
+        //Throw an runtime error telling the user that row is invalid
+        throw(std::runtime_error("Row is invalid. Must be between 0 and 9"));
+    }
+    //Otherwise, if col is less than 0 or less than 9
+    else if(col < 0 || col > 9)
+    {
+        //Throw an runtime error tellling the user that col is invalid
+        throw(std::runtime_error("Col is invalid. Must be between 0 and 9"));
+    }
+    //Otherwise, if the first character of the string at the position (row, col) is not an S
+    else if(m_visibleBoard.at(row, col)[0] != "S")
+    {
+        //Tell the user that that strike already happend at the position
+        throw(std::runtime_error("You can't mark the board at position row,col. You can only mark position that starts with S"));
+    }
+
+    //Otherwise, continue the program
+    //Firstly, we need to know the ship we are attacking
+    //Then, create an integer that will store the ship getting attacked
+    int hitship = 0;
+
+    //Next, get the ship being attack by get the second character of the position
+    hitship = m_visibleBoard.at(row, col)[1];
+
+    //Next, make the ship lose their life and check if the ship is still alive
+    //If a ship is still alive
+    if(m_ships[hitship-1].loseLife())
+    {
+        //Mark the location of the of position with strike
+        m_visibleBoard.set(row, col, strike);
+    }
+    //Otherwise, if the ship lost their life
+    else
+    {
+        //Call sinkShip and sink the ship
+        sinkShip(hitship);
+    }
+
+    //TODO: Add any messages if needed
+    //Add an error message for the specific strike if needed
+    //Add correct functions
+}
+
+//Print the healthbar for each ship
+void Player::shipHealthBar()
+{
+    //Example dislay
+    //1x1: 5     1x2: Sunked     1x3: 4  
+    
+    //Firstly, we are goning to put everything in one line for now
+    //Loop for each ship the player hass
+    for(int i = 0; i < m_numberOfShips; i++)
+    {
+        //Print out the Ship with size 1x(i+1
+        std::cout << "1x" + (i+1) + ": ";
+
+        //Next, check if the ship is stil alive
+        //TODO: Get functions for returing life and alive status
+        //If the ship is still alive
+        if(true)
+        {
+            //Print the ships life
+        }
+        //Otherwise, if the ship is not alived
+        else
+        {
+            //Display that the ship is not alive anymore
+            std::cout << "Sunked     ";
+        }
+    }
+
+    //Add the end of the loop, print a new line
+    std::cout << "\n";
+}
 
