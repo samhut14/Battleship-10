125d0ac601ab2e9aaf985c11bedd5b14d21aa605
EyeOnYou15 <96965326+EyeOnYou15@users.noreply.github.com>
2022-02-05 13:49:49 -0600

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,49 @@
+#include "Board.h"
+#include "Ship.cpp"
+#include <string>
+#include <iostream>
+using namespace std;
+Board::Board(int r, int c, string** B_arr)
+{
+    Boardarr = B_arr;
+    r = row;
+    c = col;
+    string** Boardarr = nullptr;
+    Boardarr = new string*[row];
+    for(int i = 0; i < row; i++)
+    {
+        Boardarr[i] = new string[col];
+    }
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            Boardarr[i][j] = "?";
+        }
+    }
+    
+}
+
+void Board::setRow(int rw)
+{
+    rw= row;
+}
+void Board::setColumn(int cl)
+{
+    cl = col;
+}
+int Board::getRow()
+{
+    return row;
+}
+int Board::getColumn()
+{
+    return col;
+}
+string* Board::getArr()
+{
+    return Boardarr;
+}
+
+
 
--- Board.h
+++ Board.h
@@ -1,1 +1,22 @@
-
+#ifndef BOARD_H
+#define BOARD_H
+#include <string>
+using namespace std;
+class Board
+{
+    private:
+    int row;
+    int col;
+    string** Boardarr;
+    public:
+   Board(int r, int c, string** B_arr);
+   ~Board();
+   void setRow(int rw);
+   void setColumn(int cl);
+   int getRow();
+   int getColumn();
+   string* getArr();
+   //int Boardat(int row,int col); 
+   //int Boardset(int row, int col, char element);
+};
+#endif
