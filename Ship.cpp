f54db8d7017d682852f6d966eacc892dd435ad03
Jasem Ali <sixpaths63@gmail.com>
2022-02-08 16:25:48 -0600

    Adjusted Ship's default constructor and commented out line 7

--- Ship.cpp
+++ Ship.cpp
@@ -1,20 +1,24 @@
 #include "Ship.h"
 using namespace std;
-Ship::Ship(int size, string* locationArr)
+Ship::Ship(int size, string *locationArr)
 {
     m_size = size;
     m_life = size;
-    posArr = locationArr;
+    // posArr = locationArr;
     posArr = new string[m_size];
-    for (int i = 0; i < m_size; i++) {
+    for (int i = 0; i < m_size; i++)
+    {
         posArr[i] = locationArr[i];
     }
 }
-//Ship::Ship() {}
+Ship::Ship()
+{
+    posArr = nullptr;
+}
 
 Ship::~Ship()
-{   
-   delete[] posArr;    
+{
+    delete[] posArr;
 }
 void Ship::setAlive(bool alive)
 {
@@ -23,7 +27,7 @@ void Ship::setAlive(bool alive)
 
 int Ship::getSize()
 {
-    return(m_size);
+    return (m_size);
 }
 
 int Ship::getLife()
@@ -36,17 +40,18 @@ bool Ship::getAlive()
 }
 int Ship::getColumn(int index)
 {
-    return (posArr[index].at(1)-65);
+    return (posArr[index].at(1) - 65);
 }
 int Ship::getRow(int index)
 {
-    return (posArr[index].at(0)-48);
+    return (posArr[index].at(0) - 48);
 }
-string* Ship::getPositionArr()
+string *Ship::getPositionArr()
 {
-   return posArr; 
+    return posArr;
 }
 
-bool Ship::loseLife() {
+bool Ship::loseLife()
+{
     return (--m_life == 0);
 }
